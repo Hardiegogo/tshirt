c7d455a6b152acd1e6d8cb05b2861be22b20d47e
Chirag Gupta <chirag.gps@gmail.com>
2020-09-06 02:00:19 +0530

    add full set up of category

--- Ecom_web/API/urls.py
+++ Ecom_web/API/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from rest_framework.authtoken import views
 from .views import home
 
-urlpatterns = [
+urlpatterns =[
     path('', home , name='api_home'),
-]
+    path('category/', include('API.category.urls'))
+    ]
