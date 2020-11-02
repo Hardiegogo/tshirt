665f3f5de6dc022e317d8bade90de52bcddf8c9f
Chirag Gupta <chirag.gps@gmail.com>
2020-11-03 02:29:07 +0530

    order section complete

---
+++ Ecom_web/API/order/urls.py
@@ -0,0 +1,14 @@
+from rest_framework import routers
+from django.urls import path,include
+from . import views
+from datetime import datetime
+
+
+router = routers.DefaultRouter()
+router.register(r'', views.OrderViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('add/<str:id>/<str:token>/',views.add,name="order.add")
+]
--- Ecom_web/API/order/views.py
+++ Ecom_web/API/order/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from django.contrib.auth.decorators import csrf_exempt
-from django.views.decorators.csrf import get_user_model
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
 from .models import Order
 from .serializers import OrderSerializer
 from django.http import JsonResponse
@@ -17,9 +17,9 @@ def validate_user_session_token(id, token):
         return False
     except user.DoesNotExist:
         return False
-        
+
 @csrf_exempt
-def create(request,id,token):
+def add(request,id,token):
     if not validate_user_session_token(id,token):
         return JsonResponse({'error':'Please re-Login'})
 
@@ -31,16 +31,16 @@ def create(request,id,token):
         total_products=len(products.split(','))
 
     Usermodel=get_user_model()
-     try:
-         user=Usermodel.objects.get(pk=id)
+    try:
+        user=Usermodel.objects.get(pk=id)
 
-     except user.DoesNotExist:
-         return JsonResponse({'error':'User does not exists'})
+    except user.DoesNotExist:
+        return JsonResponse({'error':'User does not exists'})
 
     ordr=Order(user=user,total_products=products,product_names=products,total_amount=amount,transaction_id=transaction_id)
     ordr.save()
     return JsonResponse({'success':True,'error':False,'msg':'Order placed successfully'})
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset=Order.Objects.all().order_by('id')
+    queryset=Order.objects.all().order_by('id')
     serializer_class=OrderSerializer
